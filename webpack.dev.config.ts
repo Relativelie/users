4b20fbcddfdcc59b22c4f8ad777dc99abd2dc9be
Relativelie <sopio1992@gmail.com>
2022-04-28 12:29:27 +0300

    bug fix

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -1,7 +1,7 @@
-import path from "path";
-import { Configuration as WebpackConfiguration, ProvidePlugin } from "webpack";
+import path from 'path';
+import { Configuration as WebpackConfiguration, ProvidePlugin } from 'webpack';
 import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 interface Configuration extends WebpackConfiguration {
@@ -9,24 +9,24 @@ interface Configuration extends WebpackConfiguration {
 }
 
 const config: Configuration = {
-    mode: "development",
+    mode: 'development',
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'dist'),
     },
-    entry: "./src/index.tsx",
+    entry: './src/index.tsx',
     module: {
         rules: [
             {
                 test: /\.(ts|js)x?$/i,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader",
+                    loader: 'babel-loader',
                     options: {
                         presets: [
-                            "@babel/preset-env",
-                            "@babel/preset-react",
-                            "@babel/preset-typescript",
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript',
                         ],
                     },
                 },
@@ -36,33 +36,33 @@ const config: Configuration = {
                 test: /\.s[ac]ss$/i,
                 use: [
                     // Creates `style` nodes from JS strings
-                    "style-loader",
+                    'style-loader',
                     // Translates CSS into CommonJS
-                    "css-loader",
+                    'css-loader',
                     // Compiles Sass to CSS
-                    "sass-loader",
+                    'sass-loader',
                 ],
             },
         ],
     },
     resolve: {
-        extensions: [".tsx", ".ts", ".js"],
+        extensions: ['.tsx', '.ts', '.js'],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./public/index.html",
+            template: './public/index.html',
         }),
         new ProvidePlugin({
-            "React": "react",
+            React: 'react',
         }),
         new CleanWebpackPlugin(),
     ],
-    devtool: "inline-source-map",
+    devtool: 'inline-source-map',
     devServer: {
-        static: path.join(__dirname, "build"),
+        static: path.join(__dirname, 'build'),
         historyApiFallback: true,
         port: 4001,
-        open: true
+        open: true,
     },
 };
 
