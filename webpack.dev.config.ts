c2cb07d7e5cafe554201b300e38fecf56585179f
Relativelie <sopio1992@gmail.com>
2022-04-28 12:10:47 +0300

    initial commit

---
+++ webpack.dev.config.ts
@@ -0,0 +1,69 @@
+import path from "path";
+import { Configuration as WebpackConfiguration, ProvidePlugin } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+interface Configuration extends WebpackConfiguration {
+    devServer?: WebpackDevServerConfiguration;
+}
+
+const config: Configuration = {
+    mode: "development",
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist'),
+    },
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript",
+                        ],
+                    },
+                },
+            },
+
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+        new ProvidePlugin({
+            "React": "react",
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    devtool: "inline-source-map",
+    devServer: {
+        static: path.join(__dirname, "build"),
+        historyApiFallback: true,
+        port: 4001,
+        open: true
+    },
+};
+
+export default config;
