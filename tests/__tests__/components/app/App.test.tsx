575209ccaf4d5eb91462cc6126b2ce9da1614bf2
Relativelie <sopio1992@gmail.com>
2022-05-05 10:19:03 +0300

    commit

--- tests/__tests__/components/app/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import App from '../../../../src/components/app/App';
-import { render } from '../../../testsSetup/test-utils';
-
-describe('App component', () => {
-    test('snapshot - app component', () => {
-        const tree = render(<App />);
-        expect(tree).toMatchSnapshot();
-    });
-});
